0c150898c344d383eaf7545f18f94223340f7095
Zack Yang <141696047+ZackYangKB@users.noreply.github.com>
2025-08-22 02:39:25 +0000

    a

---
+++ octofit-tracker/backend/octofit_tracker/management/commands/populate_db.py
@@ -0,0 +1,41 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+from octofit_tracker.models import Team, Activity, Leaderboard, Workout
+
+class Command(BaseCommand):
+    help = 'Populate the octofit_db database with test data'
+
+    def handle(self, *args, **kwargs):
+        User = get_user_model()
+        # Clear collections
+        User.objects.all().delete()
+        Team.objects.all().delete()
+        Activity.objects.all().delete()
+        Leaderboard.objects.all().delete()
+        Workout.objects.all().delete()
+
+        # Create teams
+        marvel = Team.objects.create(name='Marvel')
+        dc = Team.objects.create(name='DC')
+
+        # Create users
+        users = [
+            User.objects.create_user(username='ironman', email='ironman@marvel.com', password='pass', first_name='Tony', last_name='Stark'),
+            User.objects.create_user(username='captainamerica', email='cap@marvel.com', password='pass', first_name='Steve', last_name='Rogers'),
+            User.objects.create_user(username='batman', email='batman@dc.com', password='pass', first_name='Bruce', last_name='Wayne'),
+            User.objects.create_user(username='wonderwoman', email='wonderwoman@dc.com', password='pass', first_name='Diana', last_name='Prince'),
+        ]
+
+        # Create activities
+        Activity.objects.create(user='ironman', type='Running', duration=30)
+        Activity.objects.create(user='batman', type='Cycling', duration=45)
+
+        # Create leaderboard
+        Leaderboard.objects.create(team='Marvel', points=100)
+        Leaderboard.objects.create(team='DC', points=90)
+
+        # Create workouts
+        Workout.objects.create(name='Pushups', difficulty='Easy')
+        Workout.objects.create(name='Squats', difficulty='Medium')
+
+        self.stdout.write(self.style.SUCCESS('octofit_db populated with test data'))
